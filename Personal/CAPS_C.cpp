3d421dc6bd76120d2537ae68e3a1287ab41a0063
Secuho <secuho@secuho.life>
2025-06-23 08:13:17 +0900

    2025/06/23

---
+++ Personal/CAPS_C.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+
+int main () {
+
+printf("  oooooooo8      o      oooooooooo   oooooooo8\n");
+printf("o888     88     888      888    888 888\n");
+printf("888            8  88     888oooo88   888oooooo\n");
+printf("888o     oo   8oooo88    888                888\n");
+printf(" 888oooo88  o88o  o888o o888o       o88oooo888\n");
+
+return 0;
+}
+
+/*  oooooooo8      o      oooooooooo   oooooooo8
+o888     88     888      888    888 888
+888            8  88     888oooo88   888oooooo
+888o     oo   8oooo88    888                888
+ 888oooo88  o88o  o888o o888o       o88oooo888 */
