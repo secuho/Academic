87af8b768c119f97d77fff8b7c35e1c089867e3b
Secuho <secuho@secuho.life>
2025-06-24 05:46:02 +0900

    2025/06/24

---
+++ BaekJun_exp/10171.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+
+int main () {
+
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+
+return 0;
+}
+
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ */
